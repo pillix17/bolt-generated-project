58b472a4dad5c5ea8b4999ba03b7692e669228f5
pillix17 <48390564+pillix17@users.noreply.github.com>
2025-02-11 09:19:14 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, 'src'),
+    }
   },
+  base: './'
 })
